d0d7551b91cf50f75868c54311551665ef4b65eb
Juan Alberto Uribe Otero <juanuribeo13@gmail.com>
2012-11-13 10:41:47 -0500

    enabled javascript on help activity, now the site looks ok

--- src/com/numerical_analysis/android/activities/HelpActivity.java
+++ src/com/numerical_analysis/android/activities/HelpActivity.java
@@ -3,10 +3,12 @@ package com.numerical_analysis.android.activities;
 import com.numerical_analysis.android.R;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 
 public class HelpActivity extends Activity {
 
+	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,6 +16,7 @@ public class HelpActivity extends Activity {
 
 		String url = getIntent().getStringExtra("url");
 		WebView webView = (WebView) findViewById(R.id.webViewHelp);
+		webView.getSettings().setJavaScriptEnabled(true);
 		webView.loadUrl(url);
 	}
 }
